0900c51d04de1eadec1b6e5eb9192371c62e7665
Eliezer Steinbock <eliesteinbock@gmail.com>
2022-08-09 15:59:58 +0300

    Make options optional for log visit

--- src/index.ts
+++ src/index.ts
@@ -46,9 +46,8 @@ export async function saveAddress(options: {
   }
 }
 
-export async function logVisit(options: { referUrl?: string }) {
-  const { referUrl } = options;
-  const url = referUrl || `${DEFAULT_BASE_URI}/api/external/visit`;
+export async function logVisit(options?: { referUrl?: string }) {
+  const url = options?.referUrl || `${DEFAULT_BASE_URI}/api/external/visit`;
 
   const invitedById = getInvitedById();
   if (!invitedById) return;
